7db75d04472ae47e95114bdb4045f95a8bcde891
dewanankit <ankit.dewan2791@gmail.com>
2018-03-16 15:30:03 -0400

    Friday routine commit before function

---
+++ 02_Package/main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/dewanankit/GoLearning/02_Package/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.MyName)
+}
---
+++ 06_constants/01_constant/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+var p string = "death & taxes"
+
+func main() {
+	q := 5
+	fmt.Println(p)
+	fmt.Println(q)
+}
